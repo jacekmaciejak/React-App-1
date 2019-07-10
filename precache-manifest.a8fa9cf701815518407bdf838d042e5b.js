277e1baa48e5f1e1d6f663c8a04cc2cbac56749f
Jacek Maciejak <jacekmaciejak@poczta.fm>
2019-07-10 13:15:28 +0200

    Updates

---
+++ precache-manifest.a8fa9cf701815518407bdf838d042e5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3983de934e4134839b9ff1e84aac5cc2",
+    "url": "/React-App-1/index.html"
+  },
+  {
+    "revision": "1b14a652a8b4df9ca0d9",
+    "url": "/React-App-1/static/css/2.cfe40058.chunk.css"
+  },
+  {
+    "revision": "f9ed69fb8db212b6aa48",
+    "url": "/React-App-1/static/css/main.25bf7c3a.chunk.css"
+  },
+  {
+    "revision": "1b14a652a8b4df9ca0d9",
+    "url": "/React-App-1/static/js/2.18f6d27a.chunk.js"
+  },
+  {
+    "revision": "f9ed69fb8db212b6aa48",
+    "url": "/React-App-1/static/js/main.d97244c5.chunk.js"
+  },
+  {
+    "revision": "b574a0e39e7e155bd572",
+    "url": "/React-App-1/static/js/runtime~main.8ba5842f.js"
+  }
+]);
